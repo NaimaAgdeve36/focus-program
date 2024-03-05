af51f68d6d59aa1513982319134a6cea79079a84
Naim Ağdeve <naimagdeve@proton.me>
2024-03-06 00:38:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let  counter = 0;
+
+function count(){
+    counter++;
+    document.getElementById("sayac").innerHTML=counter;
+}
+
+function reset() {
+counter=0; 
+sayac.innerHTML=counter ;
+}
+
+
